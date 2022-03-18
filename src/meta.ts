c12c68602ece19d4f5d96d7179171f1033e0132d
Chris Kruining <c.kruining@fyn.nl>
2022-03-18 16:28:10 +0100

    whoops, fixed dependencies

--- src/meta.ts
+++ src/meta.ts
@@ -1,35 +1,35 @@
-import { readConfig } from '@remix-run/dev/config';
-import { readFile } from 'fs/promises';
-import {
-    Statement,
-    VariableStatement,
-    ObjectLiteralExpression,
-    Identifier,
-    createSourceFile,
-    ScriptTarget,
-    SyntaxKind,
-} from 'typescript';
-import expressionToLiteral from 'ts-ast-to-literal';
-import { AssetsManifest } from '@remix-run/react/entry.js';
-
-export const getConfig = async (root: string) => readConfig(root);
-
-export async function getManifest(root: string)
-{
-    const conf = await getConfig(root);
-    const file = await readFile(conf.serverBuildPath);
-    const source = createSourceFile('temp.ts', file.toString(), ScriptTarget.ESNext);
-
-    const manifest = source.statements
-        .filter((s: Statement): s is VariableStatement => s.kind === SyntaxKind.VariableStatement)
-        .flatMap(s => s.declarationList.declarations)
-        .find(d => (d.name as Identifier).escapedText === 'assets_manifest_default')
-        ?.initializer as ObjectLiteralExpression|undefined;
-
-    if(manifest === undefined)
-    {
-        throw new Error(`Unable to locate the manifest`);
-    }
-
-    return expressionToLiteral<AssetsManifest>(manifest);
-}
+// import { readConfig } from '@remix-run/dev/config';
+// import { readFile } from 'fs/promises';
+// import {
+//     Statement,
+//     VariableStatement,
+//     ObjectLiteralExpression,
+//     Identifier,
+//     createSourceFile,
+//     ScriptTarget,
+//     SyntaxKind,
+// } from 'typescript';
+// import expressionToLiteral from 'ts-ast-to-literal';
+// import { AssetsManifest } from '@remix-run/react/entry.js';
+//
+// export const getConfig = async (root: string) => readConfig(root);
+//
+// export async function getManifest(root: string)
+// {
+//     const conf = await getConfig(root);
+//     const file = await readFile(conf.serverBuildPath);
+//     const source = createSourceFile('temp.ts', file.toString(), ScriptTarget.ESNext);
+//
+//     const manifest = source.statements
+//         .filter((s: Statement): s is VariableStatement => s.kind === SyntaxKind.VariableStatement)
+//         .flatMap(s => s.declarationList.declarations)
+//         .find(d => (d.name as Identifier).escapedText === 'assets_manifest_default')
+//         ?.initializer as ObjectLiteralExpression|undefined;
+//
+//     if(manifest === undefined)
+//     {
+//         throw new Error(`Unable to locate the manifest`);
+//     }
+//
+//     return expressionToLiteral<AssetsManifest>(manifest);
+// }
