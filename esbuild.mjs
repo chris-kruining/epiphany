c78801ca504359bbad0d8fae784daab87748090f
Chris Kruining <c.kruining@fyn.nl>
2022-03-18 17:16:39 +0100

    I give up...

--- esbuild.mjs
+++ esbuild.mjs
@@ -3,12 +3,12 @@ import { build } from 'esbuild';
 const conf = {
     entryPoints: [ 'src/index.ts' ],
     outbase: 'src',
-    bundle: false,
+    bundle: true,
     sourcemap: true,
-    minify: true,
+    minify: false,
     platform: 'node',
     target: [ 'esnext' ],
-    // external: [ 'react', 'react-dom' ],
+    external: [ 'react', 'react-dom' ],
 };
 
 await build({
